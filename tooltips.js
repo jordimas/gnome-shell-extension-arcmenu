748a6395d53360cf362d5bbb9dcaf7ee5a2e7d09
Andy C <andy.c@zoho.com>
2017-06-16 00:43:35 +0100

    setings/tooltips integration

---
+++ tooltips.js
@@ -0,0 +1,12 @@
+const Mainloop = imports.mainloop;
+const St = imports.gi.St;
+const Main = imports.ui.main;
+const Lang = imports.lang;
+const Tweener = imports.ui.tweener;
+
+function Tooltip(item, initTitle) {
+    this._init(item, initTitle);
+}
+
+};
+
