1e7311ad67918d83c96368d1e19310c94d4e5a79
Alexander Rüedlinger <a.rueedlinger@gmail.com>
2017-06-18 22:24:06 +0200

    Implement "super key/keybinding" feature and "disable/enable activities hotconer" feature
    This commit introduces the following changes:
    * adds a prefs.js file that implements a base prefs widget
    * updates the metadata.json file
    * updates and renames former schema file to org.gnome.schell.extensions.arc-menu.gschema
    * adds a .gitignore file - helps to ignore compiled schema files
    * adds a logger.js - contains a base logger implementation
    * adds a helper.js - contains three helper classes:
      * MenuHotKeybinder - a helper class that helps us to bind the hotkeys Super_L and Super_R to the Arc Menu
      * KeybindingManager - a helper class that helps us to bind a custom keybinding to the Arc Menu
      * HotCornerManager - a helper class that helps us to disable/enable the activities hot corner
    
    Signed-off-by: Alexander Rüedlinger <a.rueedlinger@gmail.com>

--- widgets.js
+++ widgets.js
@@ -1,3 +1,23 @@
+/*
+ * Arc Menu: The new applications menu for Gnome 3.
+ *
+ * Copyright (C) 2017 LinxGem33. 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Import Libraries
 const Clutter = imports.gi.Clutter;
 const Lang = imports.lang;
 const Mainloop = imports.mainloop;
