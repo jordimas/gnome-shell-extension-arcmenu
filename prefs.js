f4dbc04ba1a7f156f5a1ac44f625eb19b8ad5f5d
Andy C <andy.c@zoho.com>
2017-09-03 10:20:08 +0100

    ArcMenuPreferencesWidget update for v15 #142
    this update completes the preparation for the general tab ready for new features in this category for v15.

--- prefs.js
+++ prefs.js
@@ -519,6 +519,9 @@ const ArcMenuPreferencesWidget= new GObject.Class({
         this.settings = Convenience.getSettings(Me.metadata['settings-schema']);
 
         let notebook = new PW.Notebook();
+     
+        let generalSettingsPage = new GeneralSettingsPage(this.settings);
+        notebook.append_page(generalSettingsPage, generalSettingsPage.title);
 
         let behaviourSettingsPage = new BehaviourSettingsPage(this.settings);
         notebook.append_page(behaviourSettingsPage);
