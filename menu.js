7baefcebc1208de1cb021799e8f28a93424831eb
Andrew Zaech <4151635-AndrewZaech@users.noreply.gitlab.com>
2019-07-06 20:47:01 +0000

    Fixed redisplay event when external device or bookmarks are changed.

--- menu.js
+++ menu.js
@@ -306,12 +306,6 @@ var ApplicationsButton = GObject.registerClass(
             super._onOpenStateChanged(menu, open);
         }
         _redisplayRightSide(){
-     	    if(this.network!=null)
-     		this.network.destroy();
-     	    if(this.computer!=null)
-     		this.computer.destroy();
-	    if(this.placesManager!=null){
-	        this.placesManager.destroy();} 
             this.rightBox.destroy_all_children();
             this._createRightBox();
             this.updateStyle();
@@ -634,14 +628,22 @@ var ApplicationsButton = GObject.registerClass(
 		x_fill: true,
 		y_fill: false,
 		expand:false
-	    });      
+        });      
+        
+        this._sections = { };
+       
   	    this.placesManager = new PlaceDisplay.PlacesManager();
 	    for (let i = 0; i < Constants.SECTIONS.length; i++) {
-   		let id = Constants.SECTIONS[i];
-    		this.placesManager.connect(`${id}-updated`, () => {
-			this._redisplayRightSide();
- 		});
-    	    	this._createPlaces(id);
+            let id = Constants.SECTIONS[i];
+            this._sections[id] = new St.BoxLayout({
+                vertical: true
+            });	
+            this.placesManager.connect(`${id}-updated`, () => {
+                this._redisplayPlaces(id);
+ 		    });
+
+            this._createPlaces(id);
+            this.externalDevicesBox.add(this._sections[id]);
 	    }
 
             //Add Software Shortcuts to menu (Software, Settings, Tweaks, Terminal)
@@ -723,50 +725,51 @@ var ApplicationsButton = GObject.registerClass(
             });
 
         }
-
+        _redisplayPlaces(id) {
+            this._sections[id].destroy_all_children();
+            this._createPlaces(id);
+        }
     	_createPlaces(id) {
 	      	let places = this.placesManager.get(id);
-                if (this._settings.get_boolean('show-bookmarks'))
-                {
-			if(this.placesManager.get('bookmarks').length>0)
-				this.bookmarksShorctus = true;
+                if (this._settings.get_boolean('show-bookmarks')){
+			        if(this.placesManager.get('bookmarks').length>0)
+				        this.bookmarksShorctus = true;
                 	if(id=='bookmarks' && places.length>0){
-
-        			for (let i = 0; i < places.length; i++){
-				let item = new PlaceDisplay.PlaceMenuItem(places[i],this);
-			    	this.externalDevicesBox.add(item.actor); 
-			    	} 
-			    	if(this.bookmarksShorctus && this.softwareShortcuts){
-			     		this.externalDevicesBox.add(this._createHorizontalSeparator(true), {
-						x_expand: true,
-						y_expand:false,
-						x_fill: true,
-						y_fill: false,
-						y_align: St.Align.END
-					});  
+                        for (let i = 0; i < places.length; i++){
+                            let item = new PlaceDisplay.PlaceMenuItem(places[i],this);
+                            this._sections[id].add(item.actor); 
+                        } 
+                        if(this.bookmarksShorctus && this.softwareShortcuts){
+                            this._sections[id].add(this._createHorizontalSeparator(true), {
+                                x_expand: true,
+                                y_expand:false,
+                                x_fill: true,
+                                y_fill: false,
+                                y_align: St.Align.END
+                            });  
                 		}
                 	}
-
                 }
                 if (this._settings.get_boolean('show-external-devices')){
                 	if(id!='bookmarks'){
-		        	for (let i = 0; i < places.length; i++){
-				let item = new PlaceDisplay.PlaceMenuItem(places[i],this);
-			    	this.externalDevicesBox.add(item.actor); 
-			    	this.externalDevicesShorctus=true; }
+                        for (let i = 0; i < places.length; i++){
+                            let item = new PlaceDisplay.PlaceMenuItem(places[i],this);
+                            this._sections[id].add(item.actor); 
+                            this.externalDevicesShorctus=true; 
+                        }
 			    	if(id=='network'){ 
-					if(this.externalDevicesShorctus &&  (this.bookmarksShorctus || this.softwareShortcuts)){
-		    		     		this.externalDevicesBox.add(this._createHorizontalSeparator(true), {
-							x_expand: true,
-							y_expand:false,
-							x_fill: true,
-							y_fill: false,
-							y_align: St.Align.END
-						});  
-					}
-                		}
-                	}
+                        if(this.externalDevicesShorctus &&  (this.bookmarksShorctus || this.softwareShortcuts)){
+                            this._sections[id].add(this._createHorizontalSeparator(true), {
+                                x_expand: true,
+                                y_expand:false,
+                                x_fill: true,
+                                y_fill: false,
+                                y_align: St.Align.END
+                            });  
+                        }
+                    }
                 }
+            }
     	}
 
         //used to check if a shortcut should be displayed
