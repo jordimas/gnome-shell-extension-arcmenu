2be81418607b25dc1ece2bb68ab9400d365b8c91
Andrew Zaech <4151635-AndrewZaech@users.noreply.gitlab.com>
2019-07-20 13:37:02 +0000

    Update menuWidgets.js

--- menuWidgets.js
+++ menuWidgets.js
@@ -43,6 +43,7 @@ const LoginManager = imports.misc.loginManager;
 const Gdk = imports.gi.Gdk;
 const Gtk = imports.gi.Gtk;
 const AppFavorites = imports.ui.appFavorites;
+
 const SWITCHEROO_BUS_NAME = 'net.hadess.SwitcherooControl';
 const SWITCHEROO_OBJECT_PATH = '/net/hadess/SwitcherooControl';
 const SwitcherooProxyInterface = '<node> \
@@ -50,7 +51,6 @@ const SwitcherooProxyInterface = '<node> \
   <property name="HasDualGpu" type="b" access="read"/> \
 </interface> \
 </node>';
-
 const SwitcherooProxy = Gio.DBusProxy.makeProxyWrapper(SwitcherooProxyInterface);
 // Menu Size variables
 const LARGE_ICON_SIZE = 34;
@@ -798,7 +798,14 @@ var FavoritesMenuItem = class extends BaseMenuItem {
         
         let sys = Shell.AppSystem.get_default();
         this.app = sys.lookup_app(this._command);
-
+        if(this._command == "firefox.desktop" && !this.app){
+            //check if Firefox ESR
+            if(sys.lookup_app("firefox-esr.desktop")){
+                this._command ="firefox-esr.desktop";
+                this.app = sys.lookup_app(this._command);
+                this._icon.gicon = Gio.icon_new_for_string("firefox-esr");
+            }
+        }
         this.rightClickMenu = new AppRightClickMenu(this.actor,this.app ? this.app : this._command ,this._button,true);
 
         
