229b0dbd544641c177ccc2709f247bc14cccce8b
Andrew Zaech <4151635-AndrewZaech@users.noreply.gitlab.com>
2019-10-09 13:58:56 +0000

    Top search result - deselect when mouse over other search results.

--- menuWidgets.js
+++ menuWidgets.js
@@ -1205,12 +1205,12 @@ var ApplicationMenuIcon = Utils.createClass({
             textureCache.disconnect(iconThemeChangedId);
         });
         this._updateIcon();
-
+        this.actor.connect('notify::hover', this._onHover.bind(this));
         let isMenuItem=true;
         if( app.get_description()){
             this.tooltip = new Tooltip(this.actor, app.get_description(),isMenuItem,this._button._settings);
             this.tooltip.hide();
-            this.actor.connect('notify::hover', this._onHover.bind(this));
+            
         }
         this._draggable = DND.makeDraggable(this.actor);
         this.isDraggableApp = true;
@@ -1243,11 +1243,12 @@ var ApplicationMenuIcon = Utils.createClass({
         return Clutter.EVENT_STOP;
     },
     _onHover() {
-
+        if(this._button.newSearch._highlightDefault)
+            this._button.newSearch.highlightDefault(false);
         if ( this.actor.hover) { // mouse pointer hovers over the button
-            this.tooltip.show();
+            this.tooltip ? this.tooltip.show(): '';
         } else { // mouse pointer leaves the button area
-            this.tooltip.hide();
+            this.tooltip ? this.tooltip.hide(): '';
         }
     },
     _onDragBegin() {
@@ -1481,6 +1482,7 @@ var SearchResultItem = Utils.createClass({
         this._button = button;
         this.app =app;
         this._path=path;
+        this.actor.connect('notify::hover', this._onHover.bind(this));
         if(app){
             this.icon = new IconGrid.BaseIcon(app.get_name(), {createIcon: this._createIcon.bind(this), setSizeManually: true });
             this.rightClickMenu = new AppRightClickMenu(this.actor,this.app,this._button,false, this._path ? this._path: null);
@@ -1495,6 +1497,10 @@ var SearchResultItem = Utils.createClass({
         }
      
   
+    },
+    _onHover(){
+        if(this._button.newSearch._highlightDefault)
+            this._button.newSearch.highlightDefault(false);
     },
     getDragActor() {
         return this.app.create_icon_texture(MEDIUM_ICON_SIZE);
