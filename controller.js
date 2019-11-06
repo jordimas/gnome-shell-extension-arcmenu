0ce60b8a0bb94dee6d65198e3fea6d82054c5af2
Andrew Zaech <4151635-AndrewZaech@users.noreply.gitlab.com>
2019-11-06 20:42:18 +0000

    Update Dash to Panel multi-monitor support.(Test)

--- controller.js
+++ controller.js
@@ -117,7 +117,7 @@ var MenuSettingsController = class {
         this._menuButton._updateMenuLayout();
     }
     toggleMenus(){
-        if(this._settings.get_boolean('multi-monitor')){
+        if(this._settings.get_boolean('multi-monitor') && global.dashToPanel){
             let screen = Gdk.Screen.get_default();
             //global.log( global.get_pointer());
             let pointer = global.get_pointer();
