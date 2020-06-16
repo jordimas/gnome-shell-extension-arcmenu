6c52c034658aa3f484c2be3b6e736ae4b3183ceb
Andrew Zaech <4151635-AndrewZaech@users.noreply.gitlab.com>
2020-06-16 20:33:58 +0000

    Improve mouse scrolling speed and smoothness

--- menuWidgets.js
+++ menuWidgets.js
@@ -382,8 +382,44 @@ var ScrollView = GObject.registerClass(
     class ArcMenu_ScrollView extends St.ScrollView{
     _init(params){
         super._init(params);
+        this.mouse_scroll = false;
     }
 
+    vfunc_scroll_event(event){
+        let newValue, callback, adjustment;
+        switch (event.direction){
+            case Clutter.SCROLL_SMOOTH:
+                callback = ()=> {
+                    let delta_x, delta_y;
+                    [delta_x, delta_y] = event.get_scroll_delta();
+                    this.hscroll.adjustment.adjust_for_scroll_event(delta_x);
+                    this.vscroll.adjustment.adjust_for_scroll_event(delta_y);
+                };
+                break;
+            case Clutter.ScrollDirection.DOWN:
+            case Clutter.ScrollDirection.RIGHT:
+                adjustment = event.direction === Clutter.ScrollDirection.DOWN ? this.vscroll.adjustment : this.hscroll.adjustment;
+                callback = ()=> {
+                    newValue = adjustment.value + adjustment.page_size / 6;
+                    adjustment.set_value(newValue);
+                };
+                break;
+            case Clutter.ScrollDirection.UP:
+            case Clutter.ScrollDirection.LEFT:
+                adjustment = event.direction === Clutter.ScrollDirection.UP ? this.vscroll.adjustment : this.hscroll.adjustment;
+                callback = ()=> {
+                    newValue = adjustment.value - adjustment.page_size / 6;
+                    adjustment.set_value(newValue);
+                };
+                break;
+            default:
+                break;
+        }
+        if(callback)
+            Main.initializeDeferredWork(this, callback);
+        return Clutter.EVENT_PROPAGATE;
+    }
+    
     vfunc_style_changed(){
         super.vfunc_style_changed();
         let fade = this.get_effect("fade");
@@ -658,34 +694,36 @@ var Tooltip = class ArcMenu_Tooltip{
     }
 
     _onHover() {
-        if (this.sourceActor.hover) {
-            if(this._button.tooltipShowing){
-                this.show();
-            }
-            else{
-                this._button.tooltipShowingID = GLib.timeout_add(0, 750, () => {
+        if(this._useTooltips){
+            if (this.sourceActor.hover) {
+                if(this._button.tooltipShowing){
                     this.show();
-                    this._button.tooltipShowing = true;
+                }
+                else{
+                    this._button.tooltipShowingID = GLib.timeout_add(0, 750, () => {
+                        this.show();
+                        this._button.tooltipShowing = true;
+                        this._button.tooltipShowingID = null;
+                        return GLib.SOURCE_REMOVE;
+                    });
+                }
+                if (this._button.tooltipHidingID) {
+                    GLib.source_remove(this._button.tooltipHidingID);
+                    this._button.tooltipHidingID = null;
+                }
+            }
+            else {
+                this.hide();
+                if (this._button.tooltipShowingID) {
+                    GLib.source_remove(this._button.tooltipShowingID);
                     this._button.tooltipShowingID = null;
+                }
+                this._button.tooltipHidingID = GLib.timeout_add(0, 750, () => {
+                    this._button.tooltipShowing = false;
+                    this._button.tooltipHidingID = null;
                     return GLib.SOURCE_REMOVE;
                 });
             }
-            if (this._button.tooltipHidingID) {
-                GLib.source_remove(this._button.tooltipHidingID);
-                this._button.tooltipHidingID = null;
-            }
-        } 
-        else {
-            this.hide();
-            if (this._button.tooltipShowingID) {
-                GLib.source_remove(this._button.tooltipShowingID);
-                this._button.tooltipShowingID = null;
-            }
-            this._button.tooltipHidingID = GLib.timeout_add(0, 750, () => {
-                this._button.tooltipShowing = false;
-                this._button.tooltipHidingID = null;
-                return GLib.SOURCE_REMOVE;
-            });          
         }
     }
 
